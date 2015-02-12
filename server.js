a4168c7208dd915502aa680683729f0d47c53ef6
aidarbuy <aidarbuy@gmail.com>
2015-02-12 21:28:04 +0000

    Added remote MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var doctorModel = require('./models/Doctor');
 
 var app = express();
 
@@ -7,8 +9,23 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname);
 app.set('view engine', 'jade');
 
+app.get('/api/doctors', function(req, res) {
+    mongoose.model('Doctor').find({}).exec(function(error, collection) {
+        res.send(collection);
+    });
+});
+
 app.get('*', function(req, res) {
     res.render('index');
+});
+
+// mongoose.connect('mongodb://localhost/prototype6');
+mongoose.connect('mongodb://dbuser:dbpassword@ds045001.mongolab.com:45001/prototype6');
+
+var con = mongoose.connection;
+con.once('open', function() {
+    console.log("Connected to localhost MongoDB successfully!");
+    doctorModel.seedDoctors();
 });
 
 console.log("Listening on port " + process.env.PORT);
